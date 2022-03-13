9e3c36d30dd866061e8edd674727dbe9a085d9f5
unknown <ahmedahmedmagdy491@gmail.com>
2022-03-13 14:40:51 +0200

    get token back after register

--- src/dao/users/users.dao.js
+++ src/dao/users/users.dao.js
@@ -167,6 +167,10 @@ class UserDAO {
 	static addUser(userInfo) {
 		return new Promise(async (resolve, reject) => {
 			try {
+				const user = await User.findOne({ email: userInfo.email });
+				if (user) {
+					reject(new Error('this email in use'));
+				}
 				resolve(await User.create({ ...userInfo, _id: idGenerator() }));
 			} catch (error) {
 				reject(error);
