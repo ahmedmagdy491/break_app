58c0bddf52fb3b7abbcf3efc3026a1e261882ac3
shaban7642 <ashaban7642@gmail.com>
2022-03-22 15:23:48 +0200

    remove avatar validation for agency

--- src/api/helpers/validationLayers/agency.layer.js
+++ src/api/helpers/validationLayers/agency.layer.js
@@ -1,32 +1,31 @@
 const { check } = require('express-validator');
 
 const validate = (method) => {
-	switch (method) {
-		case 'createGroup': {
-			return [
-				check('name')
-					.notEmpty()
-					.trim()
-					.withMessage('REQUIRED')
-					.bail()
-					.isString('group name must be string')
-					.bail(),
-				check('description')
-					.notEmpty()
-					.trim()
-					.withMessage('REQUIRED')
-					.isLength({ min: 20 })
-					.withMessage('description is too short')
-					.bail()
-					.isString('description must be text'),
-				check('avatar').isString('invalid image').bail(),
-			];
-		}
-		// case 'createReqJoinFromUserToJoin':{
-		// 	return [
-		// 		check('')
-		// 	]
-		// }
-	}
+    switch (method) {
+        case 'createGroup': {
+            return [
+                check('name')
+                    .notEmpty()
+                    .trim()
+                    .withMessage('REQUIRED')
+                    .bail()
+                    .isString('group name must be string')
+                    .bail(),
+                check('description')
+                    .notEmpty()
+                    .trim()
+                    .withMessage('REQUIRED')
+                    .isLength({ min: 20 })
+                    .withMessage('description is too short')
+                    .bail()
+                    .isString('description must be text'),
+            ];
+        }
+        // case 'createReqJoinFromUserToJoin':{
+        // 	return [
+        // 		check('')
+        // 	]
+        // }
+    }
 };
 module.exports = validate;
