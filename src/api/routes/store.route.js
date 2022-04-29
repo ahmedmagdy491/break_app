c6d10d7d2c3b6dfc75562dda89cc222e3995cd17
Ahmed Magdy <ahmed.magdy@binery.co>
2022-04-30 01:06:48 +0200

    add get all categories method, add products count

--- src/api/routes/store.route.js
+++ src/api/routes/store.route.js
@@ -1,14 +1,16 @@
-const StoreController = require('../controllers/store.controller');
+const StoreController = require("../controllers/store.controller");
 
-const router = require('express').Router();
+const router = require("express").Router();
 
-router.route('/addProduct').post(StoreController.addProduct);
+router.route("/addProduct").post(StoreController.addProduct);
 router
-	.route('/category')
-	.post(StoreController.addCategory)
-	.get(StoreController.getCategories);
+  .route("/category")
+  .post(StoreController.addCategory)
+  .get(StoreController.getCategories);
+
+router.route("/allCategories").get(StoreController.getCategoriesNoPagination);
 router
-	.route('/getCategoryProducts/:category_id')
-	.get(StoreController.getCategoryProducts);
+  .route("/getCategoryProducts/:category_id")
+  .get(StoreController.getCategoryProducts);
 
 module.exports = router;
--- src/dao/store.dao.js
+++ src/dao/store.dao.js
@@ -1,71 +1,83 @@
-const idGenerator = require('../api/helpers/idGenerator');
-const { Store, ProductCategory } = require('../schemas/store.schema');
+const idGenerator = require("../api/helpers/idGenerator");
+const { Store, ProductCategory } = require("../schemas/store.schema");
 
 class StoreDAO {
-	static async addProduct(productInfo) {
-		return new Promise(async (resolve, reject) => {
-			try {
-				resolve(
-					await Store.create({ ...productInfo, _id: idGenerator() })
-				);
-			} catch (error) {
-				reject(error);
-			}
-		});
-	}
+  static async addProduct(productInfo) {
+    return new Promise(async (resolve, reject) => {
+      try {
+        resolve(await Store.create({ ...productInfo, _id: idGenerator() }));
+      } catch (error) {
+        reject(error);
+      }
+    });
+  }
 
-	static async addCategory(categoryInfo) {
-		return new Promise(async (resolve, reject) => {
-			try {
-				const result = await ProductCategory.create({
-					...categoryInfo,
-					_id: idGenerator(),
-				});
-				resolve(result);
-			} catch (error) {
-				reject(error);
-			}
-		});
-	}
+  static async addCategory(categoryInfo) {
+    return new Promise(async (resolve, reject) => {
+      try {
+        const result = await ProductCategory.create({
+          ...categoryInfo,
+          _id: idGenerator(),
+        });
+        resolve(result);
+      } catch (error) {
+        reject(error);
+      }
+    });
+  }
 
-	/**
-	 * @param {number} page // number of page
-	 **/
-	static async getCategories(page) {
-		return new Promise(async (resolve, reject) => {
-			try {
-				const limit = 10;
-				const result = await ProductCategory.aggregate([
-					{ $project: { name: 1, avatar: 1 } },
-					{ $skip: (page - 1) * 2 },
-					{ $limit: 2 },
-				]);
-				resolve(result);
-			} catch (error) {
-				reject(error);
-			}
-		});
-	}
+  /**
+   * @param {number} page // number of page
+   **/
+  static async getCategories(page) {
+    return new Promise(async (resolve, reject) => {
+      try {
+        const limit = 10;
+        const result = await ProductCategory.aggregate([
+          { $project: { name: 1, avatar: 1 } },
+          { $skip: (page - 1) * 2 },
+          { $limit: 2 },
+        ]);
+        resolve(result);
+      } catch (error) {
+        reject(error);
+      }
+    });
+  }
 
-	/**
-	 * @param {string} category_id // the id of category
-	 * @param {number} page // number of page
-	 **/
-	static async getCategoryProducts(category_id, page) {
-		return new Promise(async (resolve, reject) => {
-			try {
-				const limit = 10;
-				const result = await Store.aggregate([
-					{ $match: { category: category_id } },
-					{ $skip: (page - 1) * limit },
-					{ $limit: limit },
-				]);
-				resolve(result);
-			} catch (error) {
-				reject(error);
-			}
-		});
-	}
+  static async getCategoriesNoPagination(page) {
+    return new Promise(async (resolve, reject) => {
+      try {
+        const result = await ProductCategory.aggregate([
+          { $project: { name: 1, avatar: 1 } },
+        ]);
+        resolve(result);
+      } catch (error) {
+        reject(error);
+      }
+    });
+  }
+
+  /**
+   * @param {string} category_id // the id of category
+   * @param {number} page // number of page
+   **/
+  static async getCategoryProducts(category_id, page) {
+    return new Promise(async (resolve, reject) => {
+      try {
+        const limit = 10;
+        const result = await Store.aggregate([
+          { $match: { category: category_id } },
+          { $skip: (page - 1) * limit },
+          { $limit: limit },
+        ]);
+        const count = await Store.countDocuments();
+        resolve({ result, count });
+      } catch (error) {
+        reject(error);
+      }
+    });
+  }
 }
 
 module.exports = StoreDAO;
